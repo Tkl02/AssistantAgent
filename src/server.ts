faebac56fdbf1809b7f46b0fd5c8878c7f70a3a1
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2025-07-10 11:19:45 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,23 @@
-import fastify from "fastify";
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
-import fastifyCors from "@fastify/cors";
-import { env } from "./env.ts";
+import fastifyCors from '@fastify/cors';
+import fastify from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173',
-})
+  origin: 'http://localhost:5173',
+});
 
 app.get('/teste', () => {
-    return 'teste bem feito'
-})
+  return 'teste bem feito';
+});
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.listen({ port: env.PORT }).then(() => {
-    console.log(process.env.PORT)
-    console.log('http://localhost:5173')
-})
+app.listen({ port: env.PORT }).then();
